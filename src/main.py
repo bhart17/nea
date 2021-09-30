fffad90b014b92b456f1a4db5ac31deb3993fe7f
bob <15thart@thelangton.org.uk>
2021-09-30 10:13:45 +0100

    Templating working

--- src/main.py
+++ src/main.py
@@ -1,9 +1,44 @@
 import eel
-import jinja2
+from jinja2 import Environment, FileSystemLoader, select_autoescape
 
-eel.init('web')
+WEB_PATH = "serve"
 
-eel.start('html/index.html', cmdline_args=['--start-fullscreen'], block=False)
+env = Environment(loader=FileSystemLoader(WEB_PATH),
+                  autoescape=select_autoescape())
+
+template = env.get_template("template.html")
+
+eel.init(WEB_PATH)
+
+with open("yourapp/index.html", "w") as file:
+    file.write(template.render(contents=[{
+        "type": "col",
+        "size": 1/3,
+        "content": [{
+            "type": "row",
+            "size": 0.5,
+            "content": [{
+                "type": "colour",
+                "colour": "red"
+            }]
+        }]
+    }, {
+        "type": "col",
+        "size": 1/3,
+        "content": [{
+            "type": "colour",
+            "colour": "blue"
+        }]
+    },{
+        "type": "col",
+        "size": 1/3,
+        "content": [{
+            "type": "colour",
+            "colour": "blue"
+        }]
+    }]))
+
+eel.start('index.html', cmdline_args=['--start-fullscreen'], block=False)
 
 while True:
     eel.sleep(1.0)
