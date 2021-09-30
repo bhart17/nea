08b3bd22013fc854bca349d6725cd49cefbf62e5
bhart17 <15thart@thelangton.org.uk>
2021-09-30 10:38:41 +0100

    Restructure

--- src/main.py
+++ src/main.py
@@ -1,16 +1,14 @@
 import eel
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
-WEB_PATH = "serve"
-
-env = Environment(loader=FileSystemLoader(WEB_PATH),
+env = Environment(loader=FileSystemLoader("templates"),
                   autoescape=select_autoescape())
 
 template = env.get_template("template.html")
 
-eel.init(WEB_PATH)
+eel.init("serve")
 
-with open("yourapp/index.html", "w") as file:
+with open("serve/index.html", "w") as file:
     file.write(template.render(contents=[{
         "type": "col",
         "size": 1/3,
