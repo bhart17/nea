fd6482f3c144297b4b56227b92e0a7348ace6716
bhart17 <15thart@thelangton.org.uk>
2021-09-30 14:45:44 +0100

    Text center attempt, not working

--- src/main.py
+++ src/main.py
@@ -9,32 +9,41 @@ template = env.get_template("template.tmpl")
 eel.init("serve")
 
 with open("serve/cache/index.html", "w") as file:
-    file.write(template.render(contents=[{
-        "type": "col",
-        "size": 1/3,
-        "content": [{
-            "type": "row",
-            "size": 0.5,
-            "content": [{
-                "type": "colour",
-                "colour": "red"
-            }]
-        }]
-    }, {
-        "type": "col",
-        "size": 1/3,
-        "content": [{
-            "type": "colour",
-            "colour": "blue"
-        }]
-    },{
-        "type": "col",
-        "size": 1/3,
-        "content": [{
-            "type": "colour",
-            "colour": "blue"
-        }]
-    }]))
+    file.write(
+        template.render(
+            contents=[{
+                "type": "col",
+                "size": 1 / 3,
+                "content": [{
+                    "type": "row",
+                    "size": 0.5,
+                    "content": [{
+                        "type": "colour",
+                        "content": "red"
+                    }]
+                }, {
+                    "type": "row",
+                    "size": 0.5,
+                    "content": [{
+                        "type": "text",
+                        "content": "hello"
+                    }]
+                }]
+            }, {
+                "type": "col",
+                "size": 1 / 3,
+                "content": [{
+                    "type": "colour",
+                    "content": "blue"
+                }]
+            }, {
+                "type": "col",
+                "size": 1 / 3,
+                "content": [{
+                    "type": "colour",
+                    "content": "blue"
+                }]
+            }]))
 
 eel.start('cache/index.html', cmdline_args=['--start-fullscreen'], block=False)
 
