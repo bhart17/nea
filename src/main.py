8a98ac0ce1c334d9a1a0c0b063834f17250d3f84
bhart17 <15thart@thelangton.org.uk>
2021-09-30 13:18:58 +0100

    Added cache

--- src/main.py
+++ src/main.py
@@ -4,11 +4,11 @@ from jinja2 import Environment, FileSystemLoader, select_autoescape
 env = Environment(loader=FileSystemLoader("src/templates"),
                   autoescape=select_autoescape())
 
-template = env.get_template("template.html")
+template = env.get_template("template.tmpl")
 
 eel.init("serve")
 
-with open("serve/index.html", "w") as file:
+with open("serve/cache/index.html", "w") as file:
     file.write(template.render(contents=[{
         "type": "col",
         "size": 1/3,
@@ -36,7 +36,7 @@ with open("serve/index.html", "w") as file:
         }]
     }]))
 
-eel.start('index.html', cmdline_args=['--start-fullscreen'], block=False)
+eel.start('cache/index.html', cmdline_args=['--start-fullscreen'], block=False)
 
 while True:
     eel.sleep(1.0)
