c79f7bbb31a0b16630586a8ec1cfa0d790939436
bhart17 <15thart@thelangton.org.uk>
2021-12-22 16:50:09 +0000

    Tidy up

--- src/main.py
+++ src/main.py
@@ -6,14 +6,13 @@ import json
 def main() -> None:
     SERVE_PATH = "serve"
 
-    with open("src/content.json", "r") as file:
-        content = json.load(file)
+    content = load_content()
 
     env = Environment(loader=FileSystemLoader("src/templates"),
                       autoescape=select_autoescape(enabled_extensions=()))
 
     for i in content.keys():
-        generate(env, f"{SERVE_PATH}/cache/" + content[i][0], i, content[i][1])
+        generate(env, f"{SERVE_PATH}/cache/{content[i][0]}", i, content[i][1])
 
     eel.init(SERVE_PATH)
 
@@ -25,14 +24,15 @@ def main() -> None:
         eel.sleep(1)
 
 
-def generate(env, file_p, template_p, contents) -> None:
-    template = env.get_template(template_p)
-    with open(file_p, "w") as file:
+def load_content() -> dict:
+    with open("src/content.json", "r") as file:
+        return json.load(file)
+
+
+def generate(env, fp, tp, contents) -> None:
+    template = env.get_template(tp)
+    with open(fp, "w") as file:
         file.write(template.render(contents=contents))
-
-
-def make_contents() -> dict:
-    return {}
 
 
 if __name__ == "__main__":
